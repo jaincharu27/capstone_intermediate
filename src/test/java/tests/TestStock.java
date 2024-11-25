cdf451ee071f738fdc4957f68a4d5ef1562a81bb
jaincharu27 <jcharu@virtusa.com>
2024-11-25 13:12:45 +0530

    updated test file & logs for RCOM

--- src/test/java/tests/TestStock.java
+++ src/test/java/tests/TestStock.java
@@ -12,7 +12,7 @@ public class TestStock extends BaseTest{
 	public void verify_stock_info_display() {
 		Capstone_actions stock_actions = new Capstone_actions(getDriver());
 		Allure.step("Enter stock name in search box");
-		stock_actions.enter_stock_name_in_search_box("adanient");
+		stock_actions.enter_stock_name_in_search_box("RCOM");
 		Allure.step("Select related stock from search listing");
 		stock_actions.select_stock_from_listing();
 		Allure.step("Verify navigation to stock detail page");
@@ -20,12 +20,12 @@ public class TestStock extends BaseTest{
 		Allure.step("Verify price data for 52 week High of the stock");
 		String value_52_week_high = stock_actions.get_text_52_week_high();
 		System.out.println(value_52_week_high);
-		Assert.assertEquals(value_52_week_high, "3,743.90");//2.58
+		Assert.assertEquals(value_52_week_high, "2.58");
 		log.info("Price data 52 week high matched with actual data");
 		Allure.step("Verify price data for 52 week Low of the stock");
 		String value_52_week_low = stock_actions.get_text_52_week_low();
 		System.out.println(value_52_week_low);
-		Assert.assertEquals(value_52_week_low, "2,025.00");//1.45
+		Assert.assertEquals(value_52_week_low, "1.45");
 		log.info("Price data 52 week low matched with actual data");
 	}
 }
